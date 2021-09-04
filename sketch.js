fa14fa40774bf16fe9dfec063f37e91b33741daa
Srujana2008 <86358170+Srujana2008@users.noreply.github.com>
2021-09-04 18:57:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,131 @@
+function preload(){
+  
+}
+
+function setup() {
+  createCanvas(800,500);
+  wall1 = createSprite(1200, 450, 300, 10);
+  wall1.shapeColor = "red";
+  wall2 = createSprite(950, 250, 10, 250);
+  wall3 = createSprite(560, 250, 200, 10);
+  wall4 = createSprite(400, 500, 300, 10);
+  wall5 = createSprite(570, 450, 10, 200);
+  var wall1 = createSprite(200, 25, 10, 200);
+wall1.shapeColor = 'brown';
+
+var wall2 = createSprite(650, 0, 10, 100);
+wall2.shapeColor = 'brown';
+
+var wall3 = createSprite(440,55,50, 10);
+wall3.shapeColor = 'brown';
+
+var wall4 = createSprite(421, 400, 10, 150);
+wall4.shapeColor = 'brown';
+
+var wall5 = createSprite(567, 285, 10, 125);
+wall5.shapeColor = 'brown';
+
+var wall6 = createSprite(728, 822, 100, 10);
+wall6.shapeColor = 'brown';
+
+var wall7 = createSprite(295, 220, 80, 10);
+wall7.shapeColor = 'brown';
+
+var wall8 = createSprite(555, 290, 80, 10);
+wall8.shapeColor = 'brown';
+
+var wall9 = createSprite(370, 370, 10, 76);
+wall9.shapeColor = 'brown';
+
+var wall10 = createSprite(650, 130, 10, 140);
+wall10.shapeColor = 'brown';
+
+var wall11 = createSprite(width-274, 36, 10, 70);
+wall11.shapeColor = 'brown';
+
+var wall12 = createSprite(width-40, 68, 120, 10);
+wall12.shapeColor = 'brown';
+
+var wall13 = createSprite(width-47, 180, 120, 10);
+wall13.shapeColor = 'brown';
+
+var wall14 = createSprite(width-85, 250, 60, 10);
+wall14.shapeColor = 'brown';
+
+var wall15 = createSprite(width-32, 290, 10, 110);
+wall15.shapeColor = 'brown';
+
+var wall16 = createSprite(width-167, 350, 60, 10);
+wall16.shapeColor = 'brown';
+
+var wall17 = createSprite(width-200, 200, 10, 80);
+wall17.shapeColor = 'brown';
+
+var wall18 = createSprite(width-410, 110, 120, 10);
+wall18.shapeColor = 'brown';
+
+var wall19 = createSprite(width-270, 80, 10, 60);
+wall19.shapeColor = 'brown';
+
+var wall20 = createSprite(width-340, 258, 10, 200);
+wall20.shapeColor = 'brown';
+
+var wall21 = createSprite(width-270, 165, 50, 10);
+wall21.shapeColor = 'brown';
+
+var wall22 = createSprite(width-360, 6, 50, 15);
+wall22.shapeColor = 'yellow';
+  var cardboard1 = createSprite(10,70,100,10);
+   cardboard1.shapeColor = "brown";
+  var cardboard2 = createSprite(100,50,10,100);
+  cardboard2.shapeColor = "brown";
+  var cardboard3 = createSprite(80,130,100,10);
+  cardboard3.shapeColor = "brown";
+  var cardboard4 = createSprite(50,250,10,100);
+  cardboard4.shapeColor = "brown";
+  var cardboard5 = createSprite(130,210,100,10);
+   cardboard5.shapeColor = "brown";
+  var cardboard6 = createSprite(10,250,100,10);
+   cardboard6.shapeColor = "brown";
+  var cardboard7 = createSprite(160,120,10,100);
+   cardboard7.shapeColor = "brown";
+  var cardboard8 = createSprite(150,20,100,10);
+   cardboard8.shapeColor = "brown";
+  var cardboard9 = createSprite(250,70,10,100);
+   cardboard9.shapeColor = "brown";
+  var cardboard10 = createSprite(270,150,100,10);
+   cardboard10.shapeColor = "brown";
+  var cardboard11 = createSprite(330,50,100,10);
+   cardboard11.shapeColor = "brown";
+  var cardboard12 = createSprite(340,125,10,100);
+   cardboard12.shapeColor = "brown";
+  var cardboard13 = createSprite(220,250,10,100);
+   cardboard13.shapeColor = "brown";
+  var cardboard14 = createSprite(330,210,150,10);
+   cardboard14.shapeColor = "brown";
+  var cardboard15 = createSprite(100,300,10,100);
+   cardboard15.shapeColor = "brown";
+  var cardboard16 = createSprite(180,310,100,10);
+   cardboard16.shapeColor = "brown";
+  var cardboard17 = createSprite(30,352,10,100);
+   cardboard17.shapeColor = "brown";
+  var cardboard18 = createSprite(175,352,10,100);
+   cardboard18.shapeColor = "brown";
+  var cardboard19 = createSprite(280,290,10,100);
+   cardboard19.shapeColor = "brown";
+  var cardboard20 = createSprite(350,270,120,10);
+   cardboard20.shapeColor = "brown";
+  var cardboard21 = createSprite(250,390,100,10);
+   cardboard21.shapeColor = "brown";
+  var cardboard22 = createSprite(330,370,10,100);
+   cardboard22.shapeColor = "brown";
+
+  
+
+}
+
+function draw() {
+  background("blue");  
+ 
+  drawSprites();
+}
